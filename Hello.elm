81515497eb2c5e30f9f7d4bd58b560f43d85c78b
Evan Turner <evturn@gmail.com>
2016-04-17 02:27:47 -0400

    Create mailbox

--- Hello.elm
+++ Hello.elm
@@ -1,4 +1,26 @@
-import Html
+module Main (..) where
 
-main : Html.Html
-main = Html.text "Hello"
+import Html exposing (Html)
+import Html.Events as Events
+
+
+view : Signal.Address String -> String -> Html
+view address message =
+  Html.div
+    []
+    [ Html.div [] [ Html.text message ]
+    , Html.button
+        [ Events.onClick address "Eat at Sizzler and don't ask questions"
+        ]
+        [ Html.text "Click" ]
+    ]
+
+
+mb : Signal.Mailbox String
+mb =
+  Signal.mailbox ""
+
+
+main : Signal Html
+main =
+  Signal.map (view mb.address) mb.signal
